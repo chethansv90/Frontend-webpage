579cffe5c32d8a258f7a1bb2e6d8e607a5d6b3bb
CHETHAN S V <chethansv89@gmail.com>
2023-06-24 09:31:39 +0530

    Add files via upload

---
+++ google/ext.js
@@ -0,0 +1,9 @@
+function signin(){
+    window.location="https://accounts.google.com/v3/signin/identifier?dsh=S1628030750%3A1668404135873660&authuser=0&continue=https%3A%2F%2Fwww.google.co.in%2F&ec=GAlAmgQ&hl=en&flowName=GlifWebSignIn&flowEntry=AddSession"
+}
+function search(){
+    window.location="http://127.0.0.1:5500/html/google/index.html"
+}
+function happy(){
+    window.location="https://www.google.com/doodles"
+}
